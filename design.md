cf26c79347c3ed5ada63fa973d3a3f2f849482cf
Chris Belyea <chris@chrisbelyea.com>
2021-03-30 19:33:02 -0400

    SOLUTION documentation complete; just need to add implementation instructions

--- design.md
+++ design.md
@@ -75,7 +75,17 @@ In Part 2, you will be implementing your design from Part 1. In order to get you
 
 # Design
 
-System has two components: app & database.
+The system has two components: app & database. The app is a simple Python Flask app that connects to the database. The database is a PostgreSQL cluster with Pgpool as the connection broker.
+
+In general, the solution must:
+- Pass SOC2 certification standard
+- Deploy in a repeatable and mostly automated manner across multiple Customer VPCs
+- Follow the Open/Close principle—hard to modify, but easy to extend
+
+To achieve this:
+- The solution generally aligns with SOC2 standards
+- Deployment is codified and dynamic enough to run in other customers' GCP VPCs
+- The application is "sealed" in a container image but can be extended by making derivative images and/or adding anciallary capabilities to the environment (e.g. a message queue) that connect with each other via K8s services
 
 The app must:
 
@@ -85,13 +95,15 @@ The app must:
 
 To achieve this:
 
-- App runs as a deployment on GKE
-- App has a service tied to an internal load balancer (as necessary)
-- App has a service tied to an external load balancer (as necessary)
+- App runs as a deployment scaled to multiple pods on GKE
+- For internal (VPC) clients, the app has a service tied to an internal load balancer
+- For external (non-VPC) clients, the app has an Ingress (HTTP LB)
 - Load balancers can be protected by VPC firewall rules as needed
 - The application can scale by adjusting the deployment's replica count
 - The application can scale automatically via HPA
-- The application uses K8s rolling deployments
+- The application uses K8s rolling deployments, which can be used to roll out patches or other updates
+- For use with by customers, the app container image should be versioned & published via a pipeline and distributed via a shared registry
+- **Application can be monitored**
 
 The database must:
 
@@ -101,6 +113,19 @@ The database must:
 - Dynamically configure cluster size, and machine configuration (e.g, instance types)
 
 To achieve this:
+
+- PostgreSQL cluster (StatefulSet) is deployed via Helm chart
+   - For real-world use, more performant storage should be used
+   - Database is running on K8s for the ability to scale nodes horizontally in a StatefulSet, but running on VMs may well be a better option for a number of reasons including performance and configuration flexibility
+- Pgpool facilitates connections to the cluster
+- Database nodes use PersistentVolumes for their storage, so the DB engine pod can be upgraded
+- If necessary, can take a snapshot of PersistentVolumes and mount to another system (e.g., migrate to a VM and mount the database storage)
+   - This assumes that `pg_dump` would not be performant enough for extremely large data sets, so storage snapshots would be more feasible
+   - Another potential solution to ensure consistency: Create a K8s CronJob that connects the the database, does a PostgreSQL CHECKPOINT to flush everything to disk and then calls GCP API to snapshot the underlying PV disk.
+- Helm release values can be used to alter DB configuration (ConfigMaps)
+- StatefulSet scale can be changed to change cluster size
+- Can set resource requests & limits to add CPU & memory
+- **Database can be monitored**
 
 ## Notes
 
@@ -123,6 +148,14 @@ To achieve this:
 ## Assumptions
 
 1. On-premises deployment is out of scope for this exercise. But must be deployable on GCP by customers into their VPCs.
+1. This is not intended to handle massive traffic or amounts of data but could be scaled to do so. This implementation is not multi-region.
+1. This needs to run in GCP. (Of AWS/Azure/GCP I am by far the least knowledgable about GCP so I had to do a lot of research to translate what I know from the other two.)
+
+## Future Enhancements
+1. Use CitusDB instead of PostgreSQL. This may also result in a move from running the database on K8s to running on VMs. Would need more/different bootstrapping to bring a cluster and manage nodes and data.
+1. Use DNS to provide friendly URLs for the API.
+1. Use TLS to secure API acccess. Ensure that connections to the database are encrypted.
+1. If app continues to run in K8s, potentially package as a Helm chart for easier customer adoption and to further enforce open/close principal.
 
 ## Options
 
--- docs/architecture.py
+++ docs/architecture.py
@@ -3,69 +3,91 @@ from diagrams import Cluster, Diagram
 from diagrams.gcp.network import FirewallRules, LoadBalancing
 from diagrams.k8s.compute import Deployment, Pod, ReplicaSet, StatefulSet
 from diagrams.k8s.podconfig import Secret
-from diagrams.k8s.network import Service
+from diagrams.k8s.network import Ingress, Service
 from diagrams.k8s.storage import PersistentVolume, PersistentVolumeClaim, StorageClass
 from diagrams.onprem.client import Client
 
 
-with Diagram("TRM Exercise", show=False):
-    with Cluster("GKE"):
+with Diagram("TRM Exercise Detail", show=False):
+    with Cluster("GCP"):
+        # Resources
+        app_lb_ext = LoadBalancing("App LB (HTTP/external)")
+        app_lb_int = LoadBalancing("App LB (TCP/internal)")
+        app_ingress_ext = Ingress("App Ingress (external)")
+        app_fw = FirewallRules("Firewall")
 
-        with Cluster("App Namespace"):
-            # Resources
-            app_dep = Deployment("App Deployment")
-            app_rs = ReplicaSet("App ReplicaSet")
-            app_replica_set_count = 3
-            app_pods = [Pod("App Pod") for pod in range(0, app_replica_set_count, 1)]
-            app_secret = Secret("App Secret\n(contains DB credentials & host)")
-            app_svc = Service("App Service")
-            app_lb = LoadBalancing("App LB")
-            app_fw = FirewallRules("Firewall")
+        with Cluster("GKE"):
 
-            # Relationships
-            app_dep >> app_rs >> app_pods << app_svc << app_lb << app_fw
-            app_secret << app_dep
+            with Cluster("App Namespace"):
+                # Resources
+                app_dep = Deployment("App Deployment")
+                app_rs = ReplicaSet("App ReplicaSet")
+                app_replica_set_count = 3
+                app_pods = [
+                    Pod("App Pod") for pod in range(0, app_replica_set_count, 1)
+                ]
+                app_secret = Secret("App Secret\n(contains DB credentials & host)")
+                app_svc_ext = Service("App Service (external)")
+                app_svc_int = Service("App Service (internal)")
 
-        with Cluster("DB Namespace"):
-            # Pgpool
-            # Resources
-            pgpool_dep = Deployment("Pgpool Deployment")
-            pgpool_rs = ReplicaSet("Pgpool ReplicaSet")
-            pgpool_replica_set_count = 1
-            pgpool_pods = [
-                Pod("Pgpool Pod") for pod in range(0, pgpool_replica_set_count, 1)
-            ]
-            pgpool_svc = Service("Pgpool Service")
-
-            # Relationships
-            pgpool_dep >> pgpool_rs >> pgpool_pods << pgpool_svc
-
-            # PostgreSQL
-            # Resources
-            db_sset = StatefulSet("PostgreSQL StatefulSet")
-            db_stateful_set_count = 3
-            db_svc = Service("DB Service")
-            sc = StorageClass("Storage Class (SSD)")
-
-            db_pods = []
-            for _ in range(db_stateful_set_count):
-                db_pod = Pod("DB Pod")
-                db_pvc = PersistentVolumeClaim("DB PVC")
-                db_pv = PersistentVolume("DB PV")
                 # Relationships
-                sc << db_pv << db_pvc << db_pod >> db_sset
-                db_pods.append(db_pod << db_svc)
+                app_dep >> app_rs >> app_pods << app_svc_ext << app_ingress_ext << app_lb_ext << app_fw
+                # app_pods << app_svc_int << app_lb_int << app_fw
+                app_secret << app_dep
 
-    with Cluster("Overview"):
-        # General overview of system flow
-        Client("client") >> FirewallRules("allowed source network") >> LoadBalancing(
-            "App LB"
-        ) >> Service("App Service") >> Pod("App Pod") >> Service(
-            "Pgpool Service"
-        ) >> Pod(
-            "Pgpool Pod"
-        ) >> Service(
-            "DB Service"
-        ) >> Pod(
-            "DB Pod"
-        )
+            with Cluster("DB Namespace"):
+                # Pgpool
+                # Resources
+                pgpool_dep = Deployment("Pgpool Deployment")
+                pgpool_rs = ReplicaSet("Pgpool ReplicaSet")
+                pgpool_replica_set_count = 1
+                pgpool_pods = [
+                    Pod("Pgpool Pod") for pod in range(0, pgpool_replica_set_count, 1)
+                ]
+                pgpool_svc = Service("Pgpool Service")
+
+                # Relationships
+                pgpool_dep >> pgpool_rs >> pgpool_pods << pgpool_svc
+
+                # PostgreSQL
+                # Resources
+                db_sset = StatefulSet("PostgreSQL StatefulSet")
+                db_stateful_set_count = 3
+                db_svc = Service("DB Service")
+                sc = StorageClass("Storage Class (SSD)")
+
+                db_pods = []
+                for _ in range(db_stateful_set_count):
+                    db_pod = Pod("DB Pod")
+                    db_pvc = PersistentVolumeClaim("DB PVC")
+                    db_pv = PersistentVolume("DB PV")
+                    # Relationships
+                    sc << db_pv << db_pvc << db_pod >> db_sset
+                    db_pods.append(db_pod << db_svc)
+
+        # Relationships
+        app_pods << app_svc_int << app_lb_int << app_fw
+        # app_pods >> pgpool_svc  # Not rendered since the diagram is busy enough
+
+with Diagram("TRM Exercise Overview", show=False):
+    # General overview of system flow
+
+    # Resources
+    external_client = Client("external client")
+    vpc_client = Client("vpc client")
+    app_svc_ext = Service("App Service (external)")
+    app_svc_int = Service("App Service (internal)")
+    app_lb_ext = LoadBalancing("App LB (HTTP/external)")
+    app_lb_int = LoadBalancing("App LB (TCP/internal)")
+    app_ingress_ext = Ingress("App Ingress (external)")
+    app_fw = FirewallRules("Firewall")
+    app_pod = Pod("App Pods")
+    pgpool_svc = Service("Pgpool Service")
+    pgpool_pod = Pod("Pgpool Pod")
+    db_svc = Service("DB Service")
+    db_pod = Pod("DB Pods")
+
+    # Relationships
+    external_client >> app_fw >> app_ingress_ext >> app_lb_ext >> app_svc_ext >> app_pod
+    vpc_client >> app_fw >> app_lb_int >> app_svc_int >> app_pod
+    app_pod >> pgpool_svc >> pgpool_pod >> db_svc >> db_pod
